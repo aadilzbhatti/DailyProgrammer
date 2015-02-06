c6062d458d515792343b739a6200e6122e187e19
Aadil Bhatti <aadilzbhatti@gmail.com>
2015-02-05 19:53:24 -0600

    Daily Programmer
    Reddit Daily Programmer challenges

---
+++ challenge01.py
@@ -0,0 +1,5 @@
+name = input("What is your name? ")
+age = input("What is your age? ")
+username = input("What is your Reddit username? ")
+
+print("Your name is " + name + ", your age is " + age + ", and your username is " + username + ".")
